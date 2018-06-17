a3729458de3c9ab4317c27b36c862d01275c1d30
Przemek Domel <przemyslawdomel@gmail.com>
2018-06-18 11:44:00 +1200

    Initial version

---
+++ compile.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+ gcc -I./include/ -L./lib/ \
+ ./source/rayt2d_mod.c -lpar -lcwp -lm \
+ -o /home/przemek/Desktop/for_przemyslaw/compiled/rayt2d_mod
+
+
+#Remove path from last line, leave rayt2d_mod
