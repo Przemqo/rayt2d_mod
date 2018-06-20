f3c1c5309faa9ff86ad87f70fe521f0fc6b2810b
Przemek Domel <przemyslawdomel@gmail.com>
2018-06-20 14:35:02 +1200

    Changed display for velocity and time model

---
+++ scripts/Clean.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+rm -f *.txt \
+*bin \
+*eps
+
+
---
+++ scripts/Run.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#Converting .tomo velocity format to 2d grid
+vtomo2xyz < gr_nmo_test4.tomo > gr_nmo_test4_tomo.txt 
+
+#Extracting velocities from all gridpoints 
+awk -F "\"* \"*" '{print $3*1000}' gr_nmo_test4_tomo.txt > gr_nmo_test4_tomo_vmodel.txt 
+
+#Creating velocity model
+a2b < gr_nmo_test4_tomo_vmodel.txt n1=1 > nmo_test_model.bin 
+
+#Input vel model name
+vel_model="nmo_test_model.bin"
+
+#Model view *eps
+vel_eps="Velocity_Model.eps"
+#Output from rayt2d - time model
+time_model="nmo_test_model_time.bin"
+#Time model view *eps
+time_eps="Time_Model.eps"
+
+#Clear previous, if present
+rm -f < $vel_model
+rm -f < $vel_eps
+rm -f < $time_model
+
+#Input parameters for Rayt2d
+#dt=0.004       #time sample interval in ray tracing             
+nt=802      	#number of time samples in ray tracing           
+                                                                        
+fz=0            #first depth sample in velocity                  
+nz=151        	# number of depth samples in velocity             
+dz=20         	#depth interval in velocity                      
+fx=0          	#first lateral sample in velocity                
+nx=251        	#number of lateral samples in velocity           
+dx=20         	#lateral interval in velocity    
+
+nxs=125   	    #Number of sources      &
+#fzs=0           #Depth of the sources fzs=$fzs
+#fxs=0	        #Coordinate of first source fxs=$fxs
+
+#fa=45           #First take-off angle
+#na=60           #Number of rays
+#da=2            #Increment of take-off angle fa=$fa na=$na da=$da
+
+
+#amin=0          #Minimum angle of emergence
+#amax=180        #Maximum angle of emergence amin=$amin amax=$amax
+
+#Reflector surface
+#surf="0,740;99999,740" surf=$surf
+
+
+#smooth2 n1=151 n2=251 r1=5 r2=5 < $vel_model > temp.bin
+#mv temp.bin $vel_model
+
+#Labels for model ps files
+labelz="Depth (m)"
+labelx="Distance (m)"
+labely="Shot number"
+
+#Velocity model PS file
+psimage < $vel_model  style=seismic \
+	n1=$nz d1=$dz f1=$fz grid1=dot label1="$labelz" \
+	n2=$nx d2=$dx f2=$fx grid2=dot label2="$labelx" \
+	wbox=6 hbox=4 ybox=4 \
+	d1num=1000 d2num=1000 \
+	title="Velocity"  > $vel_eps
+
+
+
+# use rayt2d to generate traveltime tables from model
+rayt2d fz=$fz nz=$nz dz=$dz fx=$fx nx=$nx dx=$dx \
+nxs=$nxs nt=$nt vfile=$vel_model tfile=$time_model 
+
+#Calculated traveltime cube PS
+pscube < $time_model n1=$nz d1=$dz f1=$fz label1="$labelz" \
+    n2=$nx d2=$dx f2=$fx label2="$labelx" \
+    n3=$nxs d3=$d3 label3="$labely" \
+	ybox=3 hbox=4 \
+	bclip=2 d1num=500 d2num=1000 d3num=20 \
+	title="Traveltime Tables"  > $time_eps
+
+
+#b2a < $time_model  n1=1 format=1 > nmo_test_model_time.txt
+#head -$nz nmo_test_model_time.txt > nmo_test_model_0offset_traveltimes.txt
+
+#Extract traveltimes for the first shot (nx x nz)
+#head -37901 nmo_test_model_time.txt > nmo_test_model_traveltimes.txt
+#a2b < nmo_test_model_traveltimes.txt n1=1 > nmo_times_shot_1.bin
+
+#View traveltimes from 1st shot
+#ximage < nmo_times_shot_1.bin n1=151 n2=251 title="OBS 3007" width=768 height=244 &
+
+
